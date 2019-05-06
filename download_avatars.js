8ab9af603dcafeda01ac95edf63cd64db3b2d219
gizemocak <gizem_ocak@outlook.com>
2019-05-06 23:28:12 +0000

    authenticated the request

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,19 @@
 var request = require('request');
+var githubToken = require('./secrets')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-    // ...
+    let options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+            'User-Agent': 'request'
+        },
+        authorization: githubToken
+    }
+    request(options, function (err, res, body) {
+        cb(err, body);
+    })
 }
 
 getRepoContributors("jquery", "jquery", function (err, result) {
